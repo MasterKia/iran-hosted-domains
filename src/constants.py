defe8e970cfb3afdd105386cb503baf1486338b3
MasterKia <MasterKia@users.noreply.github.com>
2024-01-10 22:32:07 +0330

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,6 @@
 # https://eservices.ito.gov.ir/page/iplist
 g2b_gov_url = "https://raw.githubusercontent.com/bootmortis/ito-gov-mirror/main/out/domains.csv"
-ads_url = "https://raw.githubusercontent.com/nimasaj/uBOPa/master/uBOPa_Pihole.txt"
+ads_url = "https://raw.githubusercontent.com/MasterKia/PersianBlocker/main/PersianBlockerHosts.txt"
 v2fly_base_url = "https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/"
 # https://enamad.ir/DomainListForMIMT
 enamad_url = "https://raw.githubusercontent.com/bootmortis/enamad-mirror/main/out/domains.csv"
